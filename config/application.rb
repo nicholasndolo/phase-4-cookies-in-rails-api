404fbd72b51ccaaf7eff53027888b0d29cf6fe6b
Nicholas <nicholas.nmalinda@gmail.com>
2022-10-04 20:15:25 +0300

    Cookies in APIs

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,7 @@ module Phase4CookiesInRailsApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
+   
     # Adding back cookies and session middleware
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
